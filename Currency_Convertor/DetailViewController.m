090084c69dbd46f96f012a3be2ae5c83dfdbf16b
Sermandurai <sermandurai.s@gmail.com>
2019-12-24 12:49:35 +0530

    Removed unwanted comment

--- Currency_Convertor/DetailViewController.m
+++ Currency_Convertor/DetailViewController.m
@@ -25,6 +25,7 @@
     [super viewDidLoad];
     _countryDetail = [NSArray arrayWithObjects:@"AUD",@"BGN",@"BRL",@"CAD",@"CHF",@"CNY",@"CZK",@"DKK",@"EUR",@"GBP",@"HKD",@"HRK",@"HUF",@"IDR",@"ILS",@"INR",@"ISK",@"JPY",@"KRW",@"MXN",@"MYR",@"NOK",@"NZD",@"PHP",@"PLN",@"RON",@"RUB",@"SEK",@"SGD",@"THB",@"TRY",@"USD",@"ZAR",nil];
     [self responseFromModel:[[NSUserDefaults standardUserDefaults] valueForKey:@"response"]];
+    
 }
 
 -(void)viewDidAppear:(BOOL)animated {
